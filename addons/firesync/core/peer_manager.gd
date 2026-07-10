8b38ba79f057ad8c01e6129f1208562c069a4171
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2026-07-10 13:44:58 -0300

    feat: Implement host_game method

--- addons/firesync/core/peer_manager.gd
+++ addons/firesync/core/peer_manager.gd
@@ -47,10 +47,22 @@ var max_connections: int
 var connection_timeout: float
 
 ## Read-only authoritative player register formatted as:
-## [code]{ peer_id (int): { "nickname": String, "skin_id": int } }[/code]
+## [code]{ peer_id (int): { &"name": String, ... (other metadata) } }[/code]
 var active_peers: Dictionary = {}
 ## [code]true[/code] if running as a headless server (no display/GPU).
 var is_server_dedicated: bool = false
+
+## A validator method to prevent user of send invalid or malicious metadata.
+## This method must have at least 2 parameters:
+## [br][br]    [b]1.[/b] [param peer_id]: Index of the peer in the peers list. Index
+## 1 is the host.[br]    [b]2.[/b] [param metadata]: A dictionary containing the
+## peer metadata fields. This dictionary must be filtered and returned by the
+## method.[br][br] If the validator returns an empty dictionary (invalid
+## metadata), the peer is automatically disconnected (see [method kick_peer]).
+var metadata_validator: Callable = Callable()
+## Max length of the string that identifies each peer.[br][br]Setup this value
+## on project settings ([code]firesync/network/max_peer_name_length[/code]).
+var max_peer_name_length: int = 32
 
 ## Read-only active session state.[br][br]See [enum FSSessionState].
 var _current_state: FSSessionState = FSSessionState.OFFLINE
@@ -59,13 +71,74 @@ var _network_provider: RefCounted
 
 # ------------------------------------------------------------------------------
 
+func _ready() -> void:
+	_load_project_settings()
+	is_server_dedicated = (
+			OS.has_feature("dedicated_server")
+			or DisplayServer.get_name() == "headless")
+	if is_server_dedicated:
+		host_game()
+	multiplayer.peer_connected.connect(_on_engine_peer_connected)
+
+
+func _process(delta: float) -> void:
+	pass
+
+
+func _physics_process(delta: float) -> void:
+	pass
+
+# ------------------------------------------------------------------------------
+
 ## Return the current state of a peer.[br][br]See [enum FSSessionState].
 func get_current_state() -> FSSessionState:
-	return FSSessionState.OFFLINE
+	return _current_state
 
 
 ## Commands the local peer to initialize as an authoritative session host.
-func host_game(port: int = default_port, max_players: int = max_connections) -> Error:
+func host_game(
+		port: int = default_port, max_players: int = max_connections,
+		host_metadata: Dictionary = {}) -> Error:
+	_change_state(FSSessionState.HOST_STARTING)
+
+	if not _network_provider:
+		_network_provider = ENetMultiplayerPeer.new()
+	var err: Error = _network_provider.create_server(port, max_players)
+	if not err == OK:
+		_network_provider = null
+		_change_state(FSSessionState.OFFLINE)
+		push_error(
+				"FireSync: Failed to start server on port"
+				+ " %d. Error code: %s" % [port, error_string(err)])
+		return err
+	multiplayer.multiplayer_peer = _network_provider
+
+	var final_metadata: Dictionary = {}
+	if is_server_dedicated:
+		final_metadata[&"name"] = "Server"
+	else:
+		var host_name: String = host_metadata.get(&"name", "Host").strip_edges()
+		if host_name.is_empty():
+			host_name = "Host"
+		host_metadata[&"name"] = (
+				host_name.left(max_peer_name_length).strip_edges())
+		final_metadata = host_metadata
+
+		if metadata_validator.is_valid():
+			final_metadata = await metadata_validator.call(1, final_metadata)
+		if final_metadata.is_empty():
+			_network_provider.close()
+			_network_provider = null
+			_change_state(FSSessionState.OFFLINE)
+			push_error(
+					"FireSync: The validator has rejected the metadata provided"
+					+ " by the Host.")
+			return ERR_CANT_CREATE
+
+	active_peers[1] = final_metadata
+	_change_state(FSSessionState.LOBBY_ACTIVE)
+	if not is_server_dedicated:
+		peer_registered.emit(1, final_metadata)
 	return OK
 
 
@@ -79,16 +152,43 @@ func join_game(ip: String, port: int = default_port, client_metadata: Dictionary
 func kick_peer(peer_id: int, reason: String = "") -> void:
 	pass
 
+
+## Injects a custom network provider to override the default ENet transport socket.
+func set_network_provider(provider: RefCounted) -> void:
+	if _current_state != FSSessionState.OFFLINE:
+		push_error(
+				"FireSync: Cannot change the network provider while a session "
+				+ "is active.")
+		return
+	_network_provider = provider
+
 # ------------------------------------------------------------------------------
 
 ## Reads centralized properties dynamically on startup from Project Settings.
 func _load_project_settings() -> void:
-	pass
+	default_port = ProjectSettings.get_setting(
+			FireSync.DEFAULT_PORT_SETTING_PATH,
+			FireSync.DEFAULT_PORT_DEFAULT_VALUE)
+
+	max_connections = ProjectSettings.get_setting(
+			FireSync.MAX_CONNECTIONS_SETTING_PATH,
+			FireSync.MAX_CONNECTIONS_DEFAULT_VALUE)
+
+	connection_timeout = ProjectSettings.get_setting(
+				FireSync.CONNECTION_TIMEOUT_SETTING_PATH,
+				FireSync.CONNECTION_TIMEOUT_DEFAULT_VALUE)
+
+	connection_timeout = ProjectSettings.get_setting(
+				FireSync.MAX_PEER_NAME_LENGTH_SETTING_PATH,
+				FireSync.MAX_PEER_NAME_LENGTH_DEFAULT_VALUE)
 
 
 ## Handle local machine state changes and emits matching state signals.
 func _change_state(new_state: FSSessionState) -> void:
-	pass
+	if _current_state == new_state:
+		return
+	_current_state = new_state
+	session_state_changed.emit(new_state)
 
 
 ## Registers a connected client handshake with its validated metadata.
@@ -96,3 +196,9 @@ func _change_state(new_state: FSSessionState) -> void:
 @rpc("any_peer", "reliable")
 func _register_client_handshake(metadata: Dictionary) -> void:
 	pass
+
+# ------------------------------------------------------------------------------
+
+func _on_engine_peer_connected(peer_id: int) -> void:
+	if multiplayer.is_server():
+		pass
